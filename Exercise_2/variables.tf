fd3682818567166b9542210f4f19efea15cd61d6
Irshad Nizami <irniz@rediffmail.com>
2020-12-08 17:17:30 +0530

    Update variables.tf
    Add variables for region, function name and archive location

--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,12 @@
 # TODO: Define the variable for aws_region
+variable "aws_region" {
+  type        = string
+}
+variable "function_name" {
+    type        = string
+    default = "greet_lambda"
+}
+variable "lambda_zip_location" {
+  type    = list(string)
+  default = ["outputs/greet_lambda.zip"]
+}
